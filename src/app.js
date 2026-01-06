ff93ea64ebc8effde87f8d1ea0ad34c4620b46eb
DucLuong <lt1739274@gmail.com>
2026-01-06 13:37:55 +0700

    instal nodejs

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express=require("express")
+const app=express()
+app.get("/", ()=>{
+    console.log("home page")
+})
+app.listen(3000, () => {
+    console.log("Server đang chạy cổng 3000");
+});
